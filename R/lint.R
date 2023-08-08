ae0e8f48933511fbbaebbe56010567eb2a4361ba
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-08-08 16:39:02 +0200

    big speedup with rjson + data.table

--- R/lint.R
+++ R/lint.R
@@ -5,28 +5,34 @@ lint <- function(path = ".", open = TRUE) { # TODO: add a "linter" arg
   tmp <- tempfile(fileext = ".json")
   system2("ast-grep", paste("scan --json=compact", path), stdout = tmp)
 
-  lints <- rjson::fromJSON(file = tmp)
-  browser()
-  lints <- rbindlist(lints)
-  lints[, labels := NULL]
-  lints[, metaVariables := NULL]
-  print(lints$range)
-  print(lints[, list(range = unlist(range)), by = setdiff(names(lints), 'range')])
+  lints_raw <- rjson::fromJSON(file = tmp)
+  lints <- data.frame(
+    text = sapply(lints_raw, function(x) x$text),
+    range_start_byteOffset = sapply(lints_raw, function(x) x$range$byteOffset$start),
+    range_end_byteOffset = sapply(lints_raw, function(x) x$range$byteOffset$end),
+    line_start = sapply(lints_raw, function(x) x$range$start$line),
+    col_start = sapply(lints_raw, function(x) x$range$start$column),
+    line_end = sapply(lints_raw, function(x) x$range$end$line),
+    col_end = sapply(lints_raw, function(x) x$range$end$column),
+    file = sapply(lints_raw, function(x) x$file),
+    language = sapply(lints_raw, function(x) x$language),
+    ruleId = sapply(lints_raw, function(x) x$ruleId),
+    severity = sapply(lints_raw, function(x) x$severity),
+    message = sapply(lints_raw, function(x) x$message)
+  )
+
+  replacement <- sapply(lints_raw, function(x) x$replacement)
+  if (length(replacement) > 0) {
+    lints$replacement <- replacement
+  }
 
   # clean names
   if (length(lints) == 0) return(invisible())
-  to_select <- c("text", "range.start.line", "range.start.column",
-                 "range.end.line", "range.end.column", "file",
-                 "severity", "message")
-  new_names <- c("text", "line_start", "col_start", "line_end",
+  to_select <- c("text", "line_start", "col_start", "line_end",
                  "col_end", "file", "severity",
-                 "message")
-  if ("replacement" %in% names(lints)) {
-    to_select <- c(to_select, "replacement")
-    new_names <- c(new_names, "replacement")
-  }
-  lints <- lints[, .(to_select)]
-  names(lints) <- new_names
+                 "message", "replacement")
+
+  lints <- lints[, to_select]
 
   # lines and columns locations are 0-indexed so I need to bump them
   locs <- c("line_start", "col_start", "line_end", "col_end")

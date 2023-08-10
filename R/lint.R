b4306ff465c1238d6361a0c8ef3d01bbb63b4903
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-08-10 10:56:17 +0200

    use RcppSimdJson instead

--- R/lint.R
+++ R/lint.R
@@ -5,20 +5,40 @@ lint <- function(path = ".", open = TRUE) { # TODO: add a "linter" arg
   tmp <- tempfile(fileext = ".json")
   system2("ast-grep", paste("scan --json=compact", paste(path, collapse = " ")), stdout = tmp)
 
-  lints_raw <- rjson::fromJSON(file = tmp)
-  lints <- clean_json(lints_raw)
+  lints_raw <- RcppSimdJson::fload(tmp)
+
+  if (is.null(lints_raw)) {
+    return(invisible())
+  }
+  lints <- lints_raw[, setdiff(names(lints_raw), c("metaVariables", "labels"))]
+  data.table::setDT(lints)
+
+  ranges <- data.table::rbindlist(lints$range)
+  cols <- c("byteOffset", "start", "end")
+  ranges[, (cols) := lapply(.SD, unlist), .SDcols = cols]
+  ranges[, id := rep(seq_len(nrow(ranges)/2), each = 2)]
+  ranges[, type := rep(c("row", "col"), nrow(ranges)/2)]
+  ranges <- data.table::dcast(ranges, id ~ type, value.var = cols)
+  ranges[, c("id", "byteOffset_col", "byteOffset_row") := NULL]
+  data.table::setnames(ranges, c("start_col", "start_row", "end_col", "end_row"),
+                       c("col_start", "line_start", "col_end", "line_end"))
+
+  lints[, range := NULL]
+  lints <- cbind(lints, ranges)
 
   # clean names
-  if (nrow(lints) == 0) return(invisible())
   to_select <- c("text", "line_start", "col_start", "line_end",
                  "col_end", "file", "severity",
-                 "message", "replacement")
+                 "message")
+  if ("replacement" %in% names(lints)) {
+    to_select <- c(to_select, "replacement")
+  }
 
-  lints <- lints[, to_select]
+  lints <- lints[, ..to_select]
 
   # lines and columns locations are 0-indexed so I need to bump them
   locs <- c("line_start", "col_start", "line_end", "col_end")
-  lints[locs] <- lapply(locs, function(x) lints[[x]] + 1)
+  lints[, (locs) := lapply(.SD, function(x) x + 1), .SDcols = locs]
   lints <- lints[with(lints, order(file, line_start)), ]
 
   if (isTRUE(open) &&

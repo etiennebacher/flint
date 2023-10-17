c9ad02e43193bf96fc083f4bdc70b9d52ee30911
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-10-17 09:42:42 +0200

    export setup_tinylint()

--- R/extendr-wrappers.R
+++ R/extendr-wrappers.R
@@ -12,7 +12,6 @@
 NULL
 
 #' Return string `"Hello world!"` to R.
-#' @export
 hello_world <- function() .Call(wrap__hello_world)
 
 
--- R/setup.R
+++ R/setup.R
@@ -1,5 +1,15 @@
+#' Setup tinylint
+#'
+#' This stores the default rules and internal files in `inst/tinylint`. It also
+#' imports `sgconfig.yml` that is used by `ast-grep`. This file must live at the
+#' root of the project and cannot be renamed.
+#'
+#' @return Imports files necessary for `tinylint` to work but doesn't return any
+#' value in R.
+#' @export
+
 setup_tinylint <- function() {
-  if (file.exists(".Rbuildignore")) {
+  if (fs::file_exists(".Rbuildignore")) {
     already_in <- any(grepl("sgconfig\\.yml", readLines(".Rbuildignore", warn = FALSE)))
   } else {
     already_in <- FALSE
@@ -7,27 +17,27 @@ setup_tinylint <- function() {
   if (!already_in) {
     cat(
       "# tinylint files
-^inst/rules
+^inst/tinylint
 sgconfig.yml",
       file = ".Rbuildignore",
       append = TRUE
     )
   }
   invisible(
-    file.copy(system.file("rules", package = "tinylint"), "inst")
+    fs::dir_copy(system.file("tinylint/rules", package = "tinylint"), "inst/tinylint/rules")
   )
   invisible(
-    file.copy(system.file("sgconfig.yml", package = "tinylint"), ".")
+    fs::file_copy(system.file("tinylint/sgconfig.yml", package = "tinylint"), "./sgconfig.yml")
   )
   invisible(
-    file.copy(system.file("tinylint/r.so", package = "tinylint"), "inst/tinylint")
+    fs::file_copy(system.file("tinylint/r.so", package = "tinylint"), "inst/tinylint/r.so")
   )
 }
 
 add_to_rbuildignore <- function(x) {
   cat(
     "# tinylint files
-    ^inst/rules
+    ^inst/tinylint
     sgconfig.yml",
     file = ".Rbuildignore",
     append = TRUE

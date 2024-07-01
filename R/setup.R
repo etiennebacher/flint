d0faec1165dd2da6c0a898fb3eca118f45aa6873
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-07-01 20:23:31 +0200

    remove old code

--- R/setup.R
+++ R/setup.R
@@ -10,36 +10,19 @@
 
 setup_tinylint <- function() {
   if (fs::file_exists(".Rbuildignore")) {
-    already_in <- any(grepl("sgconfig\\.yml", readLines(".Rbuildignore", warn = FALSE)))
+    already_in <- any(grepl("inst/tinylint", readLines(".Rbuildignore", warn = FALSE)))
   } else {
     already_in <- FALSE
   }
   if (!already_in) {
     cat(
       "# tinylint files
-^inst/tinylint
-sgconfig.yml",
+^inst/tinylint",
       file = ".Rbuildignore",
       append = TRUE
     )
   }
   invisible(
     fs::dir_copy(system.file("tinylint/rules", package = "tinylint"), "inst/tinylint/rules")
-  )
-  invisible(
-    fs::file_copy(system.file("tinylint/sgconfig.yml", package = "tinylint"), "./sgconfig.yml")
-  )
-  invisible(
-    fs::file_copy(system.file("tinylint/r.so", package = "tinylint"), "inst/tinylint/r.so")
-  )
-}
-
-add_to_rbuildignore <- function(x) {
-  cat(
-    "# tinylint files
-    ^inst/tinylint
-    sgconfig.yml",
-    file = ".Rbuildignore",
-    append = TRUE
   )
 }

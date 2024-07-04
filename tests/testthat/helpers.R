da3f6d3bb4ae37829933f843d75a3d01a4fc4361
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-07-04 14:17:45 +0200

    remove envvar

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -2,10 +2,7 @@ expect_lint <- function(x, message, linter) {
   if (is.null(linter)) {
     linter <- list_linters()
   }
-  withr::with_envvar(
-    new = c("FLINT_TESTING" = TRUE),
-    out <- lint_text(x, linters = linter)
-  )
+  out <- lint_text(x, linters = linter)
   if (is.null(message)) {
     testthat::expect_true(length(out) == 0)
   } else {
@@ -16,10 +13,7 @@ expect_lint <- function(x, message, linter) {
 }
 
 expect_fix <- function(x, replacement) {
-  withr::with_envvar(
-    new = c("TINYLINT_TESTING" = TRUE),
-    out <- fix_text(x)
-  )
+  out <- fix_text(x)
   testthat::expect_equal(as.character(out), replacement)
 }
 

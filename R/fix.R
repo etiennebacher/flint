4bc8237f074ea71855dfef3f57b1477e9332f74c
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-07-04 13:41:57 +0200

    working impl

--- R/fix.R
+++ R/fix.R
@@ -1,22 +1,42 @@
 #' @export
 fix <- function(path = ".", linters = NULL, open = TRUE, return_nodes = FALSE) { # TODO: add a "linter" arg
-  lints <- lint(path, linters = linters, open = open, return_nodes = TRUE)
-  browser()
-  lints <- Filter(Negate(is.null), lints)
-  args <- append(
-    list(x = astgrepr:::add_rulelist_class(lints)),
-    vapply(lints, function(x) attributes(x)$other_info$fix, character(1))
-  )
-  names(args)[2:length(args)] <- names(lints)
-  replacement2 <- as.call(append(astgrepr::node_replace_all, args)) |> eval()
 
-  root <- astgrepr::tree_new(path) |>
-    astgrepr::tree_root()
+  if (!is.null(linters) && !all(linters %in% list_linters())) {
+    stop(paste0("Unknown linters: ", toString(setdiff(linters, list_linters()))))
+  } else if (is.null(linters)) {
+    linters <- list_linters()
+  }
 
-  out <- astgrepr::tree_rewrite(root, replacement2)
-  class(out) <- c("tinylint_fix", class(out))
-  attr(out, "original") <- text
-  out
+  if (fs::is_dir(path)) {
+    r_files <- list.files(path, pattern = "\\.R$", recursive = TRUE, full.names = TRUE)
+  } else {
+    r_files <- path
+  }
+  lints <- list()
+
+  for (i in r_files) {
+    root <- astgrepr::tree_new(file = i) |>
+      astgrepr::tree_root()
+
+    files <- fs::path(system.file(package = "tinylint"), "rules/", paste0(linters, ".yml"))
+    lints_raw <- astgrepr::node_find_all(root, files = files)
+
+    if (all(lengths(lints_raw) == 0)) {
+      next
+    }
+
+    lints <- Filter(Negate(is.null), lints_raw)
+    args <- append(
+      list(x = astgrepr:::add_rulelist_class(lints)),
+      vapply(lints, function(x) attributes(x)$other_info$fix, character(1))
+    )
+    names(args)[2:length(args)] <- names(lints)
+    replacement2 <- as.call(append(astgrepr::node_replace_all, args)) |> eval()
+
+    out <- astgrepr::tree_rewrite(root, replacement2)
+    writeLines(text = out, i)
+  }
+
 }
 
 #' @export

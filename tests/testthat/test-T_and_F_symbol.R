f91c785d68bf31666867b1442c3d61ddc8eec9d9
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-07-31 15:35:54 +0200

    extra test for T_and_F_symbol in formula

--- tests/testthat/test-T_and_F_symbol.R
+++ tests/testthat/test-T_and_F_symbol.R
@@ -6,13 +6,19 @@ test_that("T_and_F_symbol_linter skips allowed usages", {
   expect_lint("x <- \"TRUE a vs FALSE b\"", NULL, linter)
 })
 
-test_that("T_and_F_symbol_linter skips usage in formulas", {
+test_that("T_and_F_symbol_linter is correct in formulas", {
   linter <- T_and_F_symbol_linter()
 
   expect_lint("lm(weight ~ T, data)", NULL, linter)
   expect_lint("lm(weight ~ T + var, data)", NULL, linter)
   expect_lint("lm(weight ~ var + var2 + T, data)", NULL, linter)
   expect_lint("lm(T ~ weight, data)", NULL, linter)
+
+  expect_lint(
+    "lm(weight ~ var + foo(x, arg = T), data)",
+    "Use TRUE instead of the symbol T.",
+    linter
+  )
 })
 
 test_that("T_and_F_symbol_linter blocks disallowed usages", {

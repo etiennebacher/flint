7137c74e274e80cbab30ff3d2c16bc5cc775da31
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-08-20 18:53:12 +0200

    Undesirable function names can be arg names, close #45

--- tests/testthat/test-undesirable_function.R
+++ tests/testthat/test-undesirable_function.R
@@ -13,3 +13,9 @@ test_that("debugonce() call detected", {
   expect_lint("debugonce()", "Function \"debugonce()\" is undesirable", linter)
   expect_lint("#debugonce()", NULL, linter)
 })
+
+test_that("those names can be used as argument names, not as values", {
+  linter <- undesirable_function_linter()
+  expect_lint("foo(browser = TRUE)", NULL, linter)
+  expect_lint("foo(x = browser())", "Function \"browser()\" is undesirable", linter)
+})

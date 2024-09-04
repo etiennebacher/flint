727b05828b6a3a5d89238423239d1c1b04780799
etiennebacher <etienne.bacher@protonmail.com>
2024-09-04 21:27:05 +0200

    add arg `overwrite` to `setup_flint_gha()`

--- R/github_action.R
+++ R/github_action.R
@@ -1,17 +1,21 @@
 #' Create a Github Actions workflow for `flint`
 #'
 #' @param path Root path to the package.
+#' @param overwrite Whether to overwrite `.github/workflows/flint.yaml` if it
+#' already exists.
 #'
 #' @return Creates `.github/workflows/flint.yaml` but doesn't return any value.
 #' @export
-setup_flint_gha <- function(path = ".") {
+setup_flint_gha <- function(path = ".", overwrite = FALSE) {
   src <- system.file("gha/flint.yaml", package = "flint")
   tar <- file.path(path, ".github/workflows/flint.yaml")
   if (!fs::dir_exists(dirname(tar))) {
     fs::dir_create(dirname(tar))
   }
-  fs::file_copy(src, tar)
-  message("Created `.github/workflows/flint.yaml.")
+  fs::file_copy(src, tar, overwrite = overwrite)
+  if (!is_testing()) {
+    message("Created `.github/workflows/flint.yaml.")
+  }
 }
 
 
--- tests/testthat/test-setup_flint_gha.R
+++ tests/testthat/test-setup_flint_gha.R
@@ -2,4 +2,7 @@ test_that("setup_flint_gha basic use works", {
   create_local_package()
   expect_no_error(suppressMessages(setup_flint_gha()))
   expect_true(file.exists(".github/workflows/flint.yaml"))
+
+  expect_error(setup_flint_gha(), "file already exists")
+  expect_no_error(setup_flint_gha(overwrite = TRUE))
 })

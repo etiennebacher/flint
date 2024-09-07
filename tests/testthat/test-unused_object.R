85dc49899bc961df937ec9af9779d8f73accf32a
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-09-07 12:24:37 +0200

    more tests for usage, do not lint functions

--- tests/testthat/test-unused_object.R
+++ tests/testthat/test-unused_object.R
@@ -3,6 +3,9 @@ test_that("unused_object_linter skips allowed usages", {
 
   expect_lint("x <- any(y)\nx", NULL, linter)
   expect_lint("x <- any(y)\n 1 + 1\nx", NULL, linter)
+
+  # functions are not linted: they could be used in other files
+  expect_lint("x <- function() 1 + 1", NULL, linter)
 })
 
 test_that("unused_object_linter blocks simple usage", {
@@ -11,9 +14,11 @@ test_that("unused_object_linter blocks simple usage", {
 
   expect_lint("x <- any(y)", msg, linter)
   expect_lint("x <- any(y)\nprint(y)", msg, linter)
+  expect_lint("x = any(y)", msg, linter)
+  expect_lint("x = any(y)\nprint(y)", msg, linter)
 })
 
-test_that("unused_object_linter works with nested usage", {
+test_that("unused_object_linter works when object is used in function", {
   linter <- unused_object_linter()
   msg <- "Object x is unused in the rest of the code"
 
@@ -30,6 +35,49 @@ test_that("unused_object_linter works with nested usage", {
   y <- function() {
     print('a')
   }
+  "
+  expect_lint(code, msg, linter)
+})
+
+test_that("unused_object_linter works when object is used in loop", {
+  linter <- unused_object_linter()
+  msg <- "Object x is unused in the rest of the code"
+
+  code <- "
+  x <- 1
+  for (i in x) print('a')
+  "
+  expect_lint(code, NULL, linter)
+
+  code <- "
+  x <- 1
+  if (i in x) print('a')
+  "
+  expect_lint(code, NULL, linter)
+
+  code <- "
+  x <- 1
+  while (x < 1) print('a')
+  "
+  expect_lint(code, NULL, linter)
+
+  # ===============================
+
+  code <- "
+  x <- 1
+  for (i in 1:2) print('a')
+  "
+  expect_lint(code, msg, linter)
+
+  code <- "
+  x <- 1
+  if (i in 1:2) print('a')
+  "
+  expect_lint(code, msg, linter)
+
+  code <- "
+  x <- 1
+  while (2 < 1) print('a')
   "
   expect_lint(code, msg, linter)
 })
